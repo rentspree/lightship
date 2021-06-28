0ccc4175f180a2e2389999c96b2e309733857943
Karnphong Kieoprasert <karnphong@rentspree.com>
2021-06-28 18:56:40 +0700

    fix: version

--- src/index.js
+++ src/index.js
@@ -25,6 +25,7 @@ const create = (
   const isTestENV = process.env.NODE_ENV === "test"
   const randomPort =
     ["true", true].includes(process.env.LIGHTSHIP_RANDOM_PORT) || isTestENV
+
   const port = randomPort ? 0 : customPort // random port
 
   const lightship = createLightShipInstance({
